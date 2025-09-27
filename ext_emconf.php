32674493949f4a763668fd13b3e928bfe22621e5
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2025-09-27 20:08:28 +0200

    Update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.9.99',
-            'bootstrap_package' => '13.0.10-14.9.99'
+            'bootstrap_package' => '13.0.0-15.9.99'
         ],
         'conflicts' => [],
         'suggests' => []
