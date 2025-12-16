92c26e742210ca9f6e474989f441ac977bae9e68
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2025-12-16 19:49:31 +0100

    Added compatibility to TYPO3 v14

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,8 +12,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.9.99',
-            'bootstrap_package' => '13.0.0-15.9.99'
+            'typo3' => '12.4.0-14.9.99',
+            'bootstrap_package' => '13.0.0-16.9.99'
         ],
         'conflicts' => [],
         'suggests' => []
