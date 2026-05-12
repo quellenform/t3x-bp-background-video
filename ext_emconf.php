7af386966f6e94d64aa7e51ecd7351dd1e5e862e
Stephan Kellermayr <stephankellermayr@users.noreply.github.com>
2026-05-12 15:57:14 +0200

    Update bootstrap_package version to 13.0.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-14.9.99',
-            'bootstrap_package' => '13.0.0-16.9.99'
+            'bootstrap_package' => '13.0.5-16.9.99'
         ],
         'conflicts' => [],
         'suggests' => []
